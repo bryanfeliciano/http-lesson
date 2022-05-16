ca76b897078453862a105ebd72be8fb4c9b21e44
bryan <bryanfeliciano892@gmail.com>
2022-05-16 14:04:44 -0500

    initial version of build request built

--- app/Main.hs
+++ app/Main.hs
@@ -17,5 +17,17 @@ noaaHost = "www.ncdc.noaa.gov"
 apiPath :: BC.ByteString
 apiPath = "/cdo-web/api/v2/datasets"
 
+buildRequest :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
+buildRequest token host method path = setRequestMethod method
+                                    $ setRequestHost host
+                                    $ setRequestHeader "token" [token]
+                                    $ setRequestPath path
+                                    $ setRequestSecure True
+                                    $ setRequestPort 443
+                                    $ defaultRequest 
+
+request :: Request
+request = buildRequest myToken noaaHost "GET" apiPath
+
 main :: IO ()
 main = print "hi"
